22e09ebcf1a2f7fa66fee7ecd5b0ba6115a760fd
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2019-09-12 23:09:47 +0200

    Updated the rest of the views to reflect the changes

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,11 +1,11 @@
 <template>
-	<div class="component card-base-layout">
-		<div>
+	<div class="component card-base-layout" v-bind:style="card_base_layout.grid_width">
+		<div v-bind:style="hide_comp_btn_order">
 			<button class="hide-component-btn" @click="hideComponent">
 				<i class="right"></i>
 			</button>
 		</div>
-		<div>
+		<div v-bind:style="card_base_layout.grid_positioning + '; grid-row-start: 1;'">
 			<h1>Contacts</h1>
 		</div>
 	</div>
@@ -15,6 +15,7 @@
 	import { eventBus } from '../main';
 
 	export default {
+		props: ['hide_comp_btn_order','card_base_layout'],
 		methods: {
 			hideComponent(){
 				eventBus.$emit('show-component', false);
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,9 +1,9 @@
 <template>
-	<div class="component card-base-layout">
-		<div>
+	<div class="component card-base-layout" v-bind:style="card_base_layout.grid_width">
+		<div v-bind:style="hide_comp_btn_order">
 			<button class="hide-component-btn" @click="hideComponent"><i class="right"></i></button>
 		</div>
-		<div class="portfolio-container">
+		<div class="portfolio-container"  v-bind:style="card_base_layout.grid_positioning + '; grid-row-start: 1;'">
 			<h1>Portfolio</h1>
 			<div class="portfolio-grid">
 				<div class="portfolio-item">
@@ -50,6 +50,7 @@
 	import { eventBus } from '../main';
 
 	export default {
+		props: ['hide_comp_btn_order','card_base_layout'],
 		methods: {
 			hideComponent(){
 				eventBus.$emit('show-component', false);
